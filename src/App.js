98b4d133a4e760c7d274fa9e8f3ed265957a56ab
tuananhhoang1805 <81757605+tuananhhoang1805@users.noreply.github.com>
2022-03-09 09:06:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Navbar from './components/Navbar/Navbar'
 import './input.css'
+import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header'
+import AboutUs from './components/AboutUs/AboutUs';
+import Menu from './components/Menu/Menu'
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header /> 
+      <AboutUs />
+      <Menu />
     </div>
   );
 }
