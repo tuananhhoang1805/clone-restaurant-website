fac7d5faa4b33e0ca99b4ff4311a159945facca4
tuananhhoang1805 <81757605+tuananhhoang1805@users.noreply.github.com>
2022-03-16 03:15:46 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './input.css'
+import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
 import AboutUs from './components/AboutUs/AboutUs';
 import Menu from './components/Menu/Menu'
+import Chef from './components/Chef/Chef'
+import Info from './components/Info/Info'
+import Laurels from './components/Laurels/Laurels';
+import Gallery from './components/Gallery/Gallery'
+import FindUs from './components/FindUs/FindUs'
+import Footer from './components/Footer/Footer'
+
+
+
 export default function App() {
   return (
     <div className="App">
@@ -10,6 +20,12 @@ export default function App() {
       <Header /> 
       <AboutUs />
       <Menu />
+      <Chef />
+      <Info />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
   );
 }
